38e050f2bff7282db172fc0abcb7ef479d89679e
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-01-09 20:38:00 +0000

    complete the method String#word_frequency

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,9 +1,12 @@
 class String
   define_method(:word_frequency) do |to_find|
+    downcase!().gsub!(/[^a-z0-9\s]/, '')
+    to_search = split()    
     frequency = 0
-    downcase!()
-    if self.==(to_find)
-      frequency = 1
+    to_search.each() do |word|
+      if word.==(to_find)
+        frequency = frequency.+(1)
+      end
     end
     frequency
   end
