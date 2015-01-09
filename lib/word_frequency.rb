9f62c896c488f29d242fe2ff6dca85176e8e170e
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-01-09 15:28:17 -0800

    add feature to find given word within words

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,12 +1,19 @@
 class String
   define_method(:word_frequency) do |to_find|
     downcase!()
-    gsub!(/[^a-z0-9\s]/, '')
+    gsub!(/[^a-z0-9\s]/, ' ')
     to_search = split()
     frequency = 0
     to_search.each() do |word|
       if word.==(to_find)
         frequency = frequency.+(1)
+      elsif word.include?(to_find)
+        number_of_times = word.split(/(?=#{to_find})/)
+        number_of_times.each() do |section|
+          if section.include?(to_find)
+            frequency = frequency.+(1)
+          end
+        end
       end
     end
     frequency
--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -5,7 +5,13 @@ describe("String#word_frequency") do
   it("counts the number of times a given word is found in a one word string") do
     expect(("Fish").word_frequency("cod")).to(eq(0))
   end
-  it("counts the number of times a given word is found in a multiple word string") do
+  it("counts the number of times a given word is found whole in a multiple word string") do
     expect(("One fish, two fish.").word_frequency("fish")).to(eq(2))
+  end
+  it("counts the number of times a given word is found as part of other words in a multiple word string") do
+    expect(("One fish, two fish, red fish, blue fishy.").word_frequency("fish")).to(eq(4))
+  end
+  it("counts the number of times a given word is repeated within a word") do
+    expect(("One fish, two fish, red fish, blue fishyfishfish.").word_frequency("fish")).to(eq(6))
   end
 end
