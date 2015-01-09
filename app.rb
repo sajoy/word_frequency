641d2c86ee75da2e4fc3ee9e686d2e3ee240dddf
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-01-09 14:13:26 -0800

    add more CSS, fix bug in String#word_frequency, add header

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
+also_reload('lib/**/*.rb')
 require('./lib/word_frequency')
 
 get('/') do
--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,7 +1,8 @@
 class String
   define_method(:word_frequency) do |to_find|
-    downcase!().gsub!(/[^a-z0-9\s]/, '')
-    to_search = split()    
+    downcase!()
+    gsub!(/[^a-z0-9\s]/, '')
+    to_search = split()
     frequency = 0
     to_search.each() do |word|
       if word.==(to_find)
