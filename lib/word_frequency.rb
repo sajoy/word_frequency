5d7b7963c1fc2b9f3ea76b263dcfe74cfc6dbd86
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-01-09 15:39:45 -0800

    remove unneeded gsub, to allow for punctuation search

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,7 +1,6 @@
 class String
   define_method(:word_frequency) do |to_find|
     downcase!()
-    gsub!(/[^a-z0-9\s]/, ' ')
     to_search = split()
     frequency = 0
     to_search.each() do |word|
